99161aeec7e1798e1e140c364c125c4a0a9d51f5
陈航 <chenhang15@maoyan.com>
2021-02-05 17:16:05 +0800

    commit

---
+++ src/main/java/cn/hangcc/automaticpunchxust/AutomaticPunchXustApplication.java
@@ -0,0 +1,13 @@
+package cn.hangcc.automaticpunchxust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticPunchXustApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomaticPunchXustApplication.class, args);
+    }
+
+}
